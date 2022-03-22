1a31bd1f4d5670bf3f61afeb41de462150969ce7
Ilya193 <ilya.komendantov@list.ru>
2022-03-22 16:52:43 +0300

    check application for open phone and coordinates

--- app/src/main/java/com/xlwe/randomuser/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/xlwe/randomuser/presentation/activities/MainActivity.kt
@@ -5,7 +5,9 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.xlwe.randomuser.R
+import com.xlwe.randomuser.databinding.ActivityMainBinding
 import com.xlwe.randomuser.presentation.OnClick
 import com.xlwe.randomuser.presentation.fragments.UserFragment
 import com.xlwe.randomuser.presentation.viewmodels.MainViewModel
@@ -13,12 +15,15 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(), OnClick {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     private val mainViewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentContainer, UserFragment.newInstance())
@@ -30,12 +35,18 @@ class MainActivity : AppCompatActivity(), OnClick {
         if (intent.resolveActivity(packageManager) != null) {
             startActivity(intent)
         }
+        else {
+            Snackbar.make(binding.root, R.string.no_apps_phone, Snackbar.LENGTH_SHORT).show()
+        }
     }
 
     override fun clickLocation(latitude: String, longitude: String) {
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:$latitude, $longitude"))
         if (intent.resolveActivity(packageManager) != null) {
             startActivity(intent)
+        }
+        else {
+            Snackbar.make(binding.root, R.string.no_apps_phone, Snackbar.LENGTH_SHORT).show()
         }
     }
 }
